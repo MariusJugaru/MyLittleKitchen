52982cfa4fdf183d55605b4eead462e5b5572777
MariusJugaru <120634011+MariusJugaru@users.noreply.github.com>
2025-07-01 14:15:27 +0300

    ItemGrabSystem
    + item pick up
    + item placement
    + item outlines

---
+++ MyLittleKitchen/Assets/Scripts/ItemHandler.cs
@@ -0,0 +1,110 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ItemHandler : MonoBehaviour
+{
+    [Header("Camera")]
+    public Camera cam;
+    public float maxDistToItem;
+
+    [Header("Hands")]
+    public GameObject leftHand;
+    public GameObject rightHand;
+
+    private RaycastHit hit;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Transform item = rightHand.transform.childCount > 0 ? rightHand.transform.GetChild(0) : null;
+
+            if (item != null)
+            {
+                PlaceItem(rightHand, item);
+                return;
+            }
+            PickItem(rightHand);
+        }
+
+    }
+
+    private void PlaceItem(GameObject hand, Transform item)
+    {
+        if (hand == null)
+        {
+            Debug.Log("Hand is not valid.");
+            return;
+        }
+
+        // Raycast from player camera to mouse position
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, maxDistToItem))
+        {
+            Vector3 hitpoint = hit.point;
+            Vector3 newPos = hitpoint + new Vector3(0, item.localScale.y / 2f + 0.1f, 0);
+
+            // Place item at the mouse position
+            item.SetParent(null, worldPositionStays: false);
+            item.localPosition = newPos;
+
+            // Set new rotation
+            Vector3 currentRotItem = item.rotation.eulerAngles;
+            Vector3 currentRotCam = cam.transform.rotation.eulerAngles;
+            Vector3 newRotation = new Vector3(currentRotItem.x, currentRotCam.y, currentRotItem.z);
+
+            item.rotation = Quaternion.Euler(newRotation);
+
+            BoxCollider box = item.GetComponent<BoxCollider>();
+            Rigidbody body = item.GetComponent<Rigidbody>();
+
+            // activate boxCollider and rigidbody
+            if (box != null)
+                box.enabled = true;
+            if (body != null)
+                body.isKinematic = false;
+
+            Debug.Log(hitpoint);
+        }
+    }
+
+    // Picks up 
+    private void PickItem(GameObject hand)
+    {
+        if (hand == null)
+        {
+            Debug.Log("Hand is not valid.");
+            return;
+        }
+
+        // Raycast from player camera to mouse position
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, maxDistToItem))
+        {
+            BoxCollider box = hit.transform.GetComponent<BoxCollider>();
+            Rigidbody body = hit.transform.GetComponent<Rigidbody>();
+
+            // deactivate boxCollider and rigidbody
+            if (box != null)
+                box.enabled = false;
+            if (body != null)
+                body.isKinematic = true;
+
+            // move item into the hand
+            hit.transform.SetParent(hand.transform, worldPositionStays: false);
+            hit.transform.localPosition = Vector3.zero;
+            hit.transform.localRotation = Quaternion.identity;
+
+            Debug.Log(hit.transform);
+        }
+    }
+}
