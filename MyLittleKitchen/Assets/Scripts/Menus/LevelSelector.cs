594f79fe7cc90f1abeb5e81636fcef3266ecaea9
MariusJugaru <120634011+MariusJugaru@users.noreply.github.com>
2025-07-31 10:46:39 +0300

    New level
    + added the bacon level
    + now it's easier to customize new levels

--- MyLittleKitchen/Assets/Scripts/Menus/LevelSelector.cs
+++ MyLittleKitchen/Assets/Scripts/Menus/LevelSelector.cs
@@ -1,11 +1,33 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelSelector : MonoBehaviour
 {
-    
+    public List<ServeFoodScript.PlateRequirements> allPlates;
+    public String currentLevel;
+
     public void PlayLevel1()
     {
+        SceneManager.sceneLoaded += OnSceneLoaded;
         SceneManager.LoadScene("MainGame");
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "MainGame")
+        {
+            GameObject servingStation = GameObject.Find("ServingStation");
+
+            if (servingStation)
+            {
+                ServeFoodScript serveFood = servingStation.GetComponent<ServeFoodScript>();
+                serveFood.allPlates = allPlates;
+                serveFood.currentLevel = currentLevel;
+            }
+        }
+
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 }
